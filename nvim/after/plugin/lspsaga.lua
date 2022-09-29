f3c3617d29f693f183914367d87774863ecc2d5a
Alan Hughes <hughesno1@gmail.com>
2022-09-29 09:24:42 +0100

    keymaps

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -16,7 +16,7 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 vim.keymap.set("n", "[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("n", "<leader>gf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+vim.keymap.set("n", "gf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
 vim.keymap.set("i", "<C-s>", "<Cmd>Lspsaga signature_help<CR>", opts)
 vim.keymap.set("n", "<C-s>", "<Cmd>Lspsaga signature_help<CR>", opts)
 vim.keymap.set("n", "<leader>cd", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
