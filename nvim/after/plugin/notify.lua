855f1209afdd8e6566b65815ad1bb9e8f1aa6c33
Alan Hughes <hughesno1@gmail.com>
2023-04-03 08:46:19 +0100

    update

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -1,33 +1,33 @@
 local status_ok, notify = pcall(require, "notify")
 if not status_ok then
-	return
+  return
 end
 
 local icons = require("user.icons")
 
 notify.setup({
-	stages = "fade_in_slide_out",
-	on_open = nil,
-	on_close = nil,
-	render = "default",
-	background_colour = "Normal",
-	minimum_width = 10,
-	icons = {
-		ERROR = icons.diagnostics.Error,
-		WARN = icons.diagnostics.Warning,
-		INFO = icons.diagnostics.Information,
-		DEBUG = icons.ui.Bug,
-		TRACE = icons.ui.Pencil,
-	},
+  stages = "fade_in_slide_out",
+  on_open = nil,
+  on_close = nil,
+  render = "default",
+  background_colour = "#000000",
+  minimum_width = 10,
+  icons = {
+    ERROR = icons.diagnostics.Error,
+    WARN = icons.diagnostics.Warning,
+    INFO = icons.diagnostics.Information,
+    DEBUG = icons.ui.Bug,
+    TRACE = icons.ui.Pencil,
+  },
 })
 
 vim.notify = notify
 
 local notify_filter = vim.notify
 vim.notify = function(msg, ...)
-	if msg:match("character_offset must be called") then
-		return
-	end
+  if msg:match("character_offset must be called") then
+    return
+  end
 
-	notify_filter(msg, ...)
+  notify_filter(msg, ...)
 end
