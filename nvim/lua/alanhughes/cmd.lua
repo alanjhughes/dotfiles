14fccfc2fc67009bdc066317cd648e8e1828ec97
Alan Hughes <hughesno1@gmail.com>
2023-11-05 21:43:50 +0000

    update

--- nvim/lua/alanhughes/cmd.lua
+++ nvim/lua/alanhughes/cmd.lua
@@ -13,16 +13,3 @@ vim.cmd([[
     autocmd BufNewfile,BufRead Podfile,*.podspec set ft=ruby
   augroup end
 ]])
-
-vim.cmd([[
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost * FormatWrite
-augroup END
-]])
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
