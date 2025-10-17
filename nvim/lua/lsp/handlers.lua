c056057b0e228371c790c9bd1fd30c44a1a9517e
Alan Hughes <alan@expo.io>
2025-10-17 18:34:39 +0100

    update

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -1,22 +1,18 @@
 local M = {}
 
 M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
-
   local config = {
     virtual_text = {
       spacing = 5,
     },
-    signs = true,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.HINT] = "",
+        [vim.diagnostic.severity.INFO] = "",
+      },
+    },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
